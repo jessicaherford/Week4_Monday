f1daf75efc08f35f2303e3af48aa89ce60dcad3d
calebatwood <calebatwood@Calebs-MacBook-Air.local>
2015-10-05 15:08:59 -0600

    CSS updating

--- jQuerying.js
+++ jQuerying.js
@@ -1,17 +1,1 @@
-$(document).ready(function(){
 
-  $("#hover").mouseenter(function(){
-    $("#hover").css("background-color", "red");
-  });
-
-  $("#hover").mouseleave(function(){
-    $("#hover").css("background-color", "#39B7CD");
-  });
-
-
-
-
-
-
-
-});
