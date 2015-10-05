87afa1577cf27c6fd520c636b9afec0652907231
calebatwood <calebatwood@Calebs-MacBook-Air.local>
2015-10-05 12:59:53 -0600

    Edit exercise

--- main.js
+++ main.js
@@ -4,34 +4,19 @@
 // After you've completed an exercise, comment out your code (unless otherwise indicated).
 
 // 1. Hide all of the paragraphs on the index.html page.
-// $(document).ready(function(){
-//     $("p").hide();
-// });
+
 
 // 2. Hide all of the h1's on the page.
 
-// $(document).ready(function(){
-//     $("h1").hide();
-// });
 
 // 3. Hide all of the elements in the document.
-//
-// $(document).ready(function(){
-//     $("*").hide();
-// });
+
 
 // 4. Hide all links on the page
-//
-// $(document).ready(function(){
-//     $("a").hide();
-// });
+
 
 // 5. Hide all of the odd table rows (Austin, Colin, & Jessica's rows)
-// $(document).ready(function(){
-//     $("tr:odd").hide();
-// });
+
+
 
 // 5. Hide all paragraphs when they're clicked.
-// $("p").click(function(){
-//     $(this).hide();
-// });
